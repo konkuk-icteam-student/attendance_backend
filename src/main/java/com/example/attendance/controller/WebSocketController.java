c54269143dabdbddfe97d50bfae7ee5da4955cd8
junhyung park <87253932+junhyungdev@users.noreply.github.com>
2024-02-08 11:17:23 +0900

    feat:: socket testing with deptId=1

--- src/main/java/com/example/attendance/controller/WebSocketController.java
+++ src/main/java/com/example/attendance/controller/WebSocketController.java
@@ -1,3 +1,38 @@
+//package com.example.attendance.controller;
+//
+//import com.example.attendance.model.dto.DeptIdRequest;
+//import com.example.attendance.model.dto.UserInfo;
+//import com.example.attendance.service.UserService;
+//import com.example.attendance.service.WebSocketService;
+//import jakarta.persistence.EntityNotFoundException;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.messaging.handler.annotation.MessageMapping;
+//import org.springframework.messaging.handler.annotation.Payload;
+//import org.springframework.messaging.handler.annotation.SendTo;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.Collections;
+//import java.util.List;
+//
+//@RequiredArgsConstructor
+//@Controller
+//public class WebSocketController {
+//    public final UserService userService;
+//    @MessageMapping("/send-attendance")
+//    @SendTo("/topic/attendance/dept/{deptId}")
+//    public List<UserInfo> sendAttendance(@Payload DeptIdRequest deptIdRequest) {
+//        Long deptId = deptIdRequest.getDeptId();
+//        try {
+//            List<UserInfo> currentAttendanceUsers = userService.getCurrentAttendanceUsers(deptId);
+//            WebSocketService webSocketService = null;
+//            webSocketService.sendCurrentAttendanceUsers(deptId, currentAttendanceUsers);
+//            return currentAttendanceUsers;
+//        } catch (EntityNotFoundException e) {
+//            return Collections.emptyList();
+//        }
+//    }
+//}
 package com.example.attendance.controller;
 
 import com.example.attendance.model.dto.DeptIdRequest;
@@ -6,6 +41,7 @@ import com.example.attendance.service.UserService;
 import com.example.attendance.service.WebSocketService;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -19,17 +55,25 @@ import java.util.List;
 @Controller
 public class WebSocketController {
     public final UserService userService;
-    @MessageMapping("/send-attendance")
-    @SendTo("/topic/attendance/dept/{deptId}")
+//    @MessageMapping("/dept")
+//    @SendTo("/topic/currentMember/{deptId}")
+//    public List<UserInfo> sendAttendance(@Payload DeptIdRequest deptIdRequest,@DestinationVariable Long deptId) {
+//        try {
+//            List<UserInfo> currentAttendanceUsers = userService.getCurrentAttendanceUsers(deptIdRequest.getDeptId());
+//            return currentAttendanceUsers;
+//        } catch (EntityNotFoundException e) {
+//            return Collections.emptyList();
+//        }
+//    }
+    @MessageMapping("/dept")
+    @SendTo("/topic/currentMember/1")
     public List<UserInfo> sendAttendance(@Payload DeptIdRequest deptIdRequest) {
-        Long deptId = deptIdRequest.getDeptId();
         try {
-            List<UserInfo> currentAttendanceUsers = userService.getCurrentAttendanceUsers(deptId);
-            WebSocketService webSocketService = null;
-            webSocketService.sendCurrentAttendanceUsers(deptId, currentAttendanceUsers);
+            List<UserInfo> currentAttendanceUsers = userService.getCurrentAttendanceUsers(deptIdRequest.getDeptId());
             return currentAttendanceUsers;
         } catch (EntityNotFoundException e) {
             return Collections.emptyList();
         }
-    }
 }
+}
+
