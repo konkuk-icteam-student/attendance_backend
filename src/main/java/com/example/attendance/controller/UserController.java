c4c3bfa45d864afc08563aaae50d622d043cae18
sejoon00 <parksejoon313@gmail.com>
2024-03-28 16:26:11 +0900

    test

--- src/main/java/com/example/attendance/controller/UserController.java
+++ src/main/java/com/example/attendance/controller/UserController.java
@@ -35,7 +35,6 @@ public class UserController {
 
     //public final UserSemesterService userSemesterService;
 
-
     @PostMapping("/new-user")
     public ResponseEntity<Object> saveUser(@RequestBody UserCreateRequest request){
         try{
