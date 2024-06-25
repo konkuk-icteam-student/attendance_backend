a4acf26b945df2bc40fc50bbd75fb48e0a54feff
박세준 <74056843+sejoon00@users.noreply.github.com>
2024-06-25 13:36:35 +0900

    Fix: cors webconfig

--- src/main/java/com/example/attendance/config/WebConfig.java
+++ src/main/java/com/example/attendance/config/WebConfig.java
@@ -1,20 +1,24 @@
 package com.example.attendance.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")  // 모든 경로에 대해 적용
-                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("http://114.70.23.79:3000")// 허용할 오리진 지정
-                .allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메소드 지정
-                .allowCredentials(true)
-                .allowedHeaders("*")
-                .maxAge(3600);
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://114.70.23.79:3000", "http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
